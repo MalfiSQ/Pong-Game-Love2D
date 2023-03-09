4d31b747eccb481c8a5b68fc43e86a22ba07b187
Malik Fattaev <malfisq@gmail.com>
2023-03-09 13:24:23 +0500

    Changed files | 1

--- main.lua
+++ main.lua
@@ -15,3 +15,12 @@ function love.draw()
     player.draw()
     ball.draw()
 end
+
+-- Functions | AABB Collision
+function checkCollision(a, b)
+    if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
+        return true
+    else
+        return false
+    end
+end
--- src/ball.lua
+++ src/ball.lua
@@ -12,6 +12,7 @@ end
 
 function ball.update(dt)
     ballMove(dt)
+    ballCollide(dt)
 end
 
 function ball.draw()
@@ -22,4 +23,22 @@ end
 function ballMove(dt)
     ball.x = ball.x + ball.xVel * dt
     ball.y = ball.y + ball.yVel * dt
+end
+
+function ballCollide(dt)
+    if checkCollision(ball, player) then
+        ball.xVel = ball.speed
+        local middleBall = ball.y + ball.height / 2
+        local middlePlayer = player.y + player.height / 2
+        local collisionPosition = middleBall - middlePlayer
+        ball.yVel = collisionPosition * 5
+    end
+
+    if ball.y < 0 then
+        ball.y = 0
+        ball.yVel = -ball.yVel
+    elseif ball.y + ball.height > love.graphics.getHeight() then
+        ball.y = love.graphics.getHeight() - ball.height
+        ball.yVel = -ball.yVel
+    end
 end
